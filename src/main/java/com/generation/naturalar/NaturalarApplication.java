a5820109bb9b66dcb42827427bae8b92ae2c8ab4
Alan <alan-hd@hotmail.com>
2024-04-01 15:30:14 -0300

    Iniciação projeto

---
+++ src/main/java/com/generation/naturalar/NaturalarApplication.java
@@ -0,0 +1,13 @@
+package com.generation.naturalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/naturalar/NaturalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.naturalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
