930489e966bca8d450f78d0049e50f1ab6ac35da
Maria Xavier <117415320+marii182@users.noreply.github.com>
2024-05-14 10:59:10 -0300

    Update ProdutoController.java

--- src/main/java/com/generation/naturalar/controller/ProdutoController.java
+++ src/main/java/com/generation/naturalar/controller/ProdutoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.naturalar.model.Produto;
@@ -26,6 +27,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
